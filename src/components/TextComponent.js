508aee6e92b0867ae8e0efc3cc52c43f562e70a1
shobdestiny1 <shobhit@0xytocin.com>
2022-03-08 23:48:03 +0530

    Latest changes

--- src/components/TextComponent.js
+++ src/components/TextComponent.js
@@ -1,178 +1,298 @@
 import React, { useState, useEffect } from "react";
-import { connectWalletHandler, isPrivateTime, isGenesisTime ,isWhiteTime, isWhiteListed, isPrivateListed, isGenesisHolder, privateSale, whitelistSale, genesisSale, privateBought, whitelistBought, genesisBought } from "./../contract/contractInteraction";
+import {
+  connectWalletHandler,
+  isPrivateTime,
+  isGenesisTime,
+  isWhiteTime,
+  isWhiteListed,
+  isPrivateListed,
+  isGenesisHolder,
+  privateSale,
+  whitelistSale,
+  genesisSale,
+  privateBought,
+  whitelistBought,
+  genesisBought,
+} from "./../contract/contractInteraction";
 
 const TextComponent = (props) => {
-
   const [privateListed, setPrivateListed] = useState(false);
   const [whiteListed, setWhiteListed] = useState(false);
   const [genesisHolder, setGenesisHolder] = useState(false);
-
   const [privateBoughtInitiated, setPrivateBought] = useState(false);
   const [whitelistBoughtInitiated, setWhitelistBoughtRes] = useState(false);
-  const [genesisBoughtInitiated, setGenesisBoughtRes] = useState(false);
-
+  const [genesisBoughtInitiated, setGenesisBoughtRes] = useState(null);
   const [connected, setConnected] = useState(false);
-
   const [minted, setMinted] = useState(false);
-
-  const [textMessage, setTextMessage] = useState("")
-
-  const [loader, setLoader] = useState(false)
-
-  const [walletAddress, setWalletAddress] = useState("")
-  const [exactAddress, setExactAddress] = useState("hbdcjbdwjbvjb")
+  const [textMessage, setTextMessage] = useState("");
+  const [loader, setLoader] = useState(false);
+  const [walletAddress, setWalletAddress] = useState("");
+  const [exactAddress, setExactAddress] = useState("hbdcjbdwjbvjb");
 
   useEffect(() => {
-    if(connected === true){
+    if (connected === true) {
       check();
     }
   }, [connected]);
+  console.log(genesisBoughtInitiated, "hgfdsdfghjgyftdrf");
 
   const check = async () => {
-    setLoader(true)
-    let genesisBoughtRes = []
-    let isPrivateListedRes = await isPrivateListed(exactAddress)
-    setPrivateListed(isPrivateListedRes)
-    let privateBoughtRes = await privateBought(exactAddress)
-    setPrivateBought(privateBoughtRes)
-    let isWhiteListedRes = await isWhiteListed(exactAddress)
-    setWhiteListed(isWhiteListedRes)
-    let whitelistBoughtRes = await whitelistBought(exactAddress)
-    setWhitelistBoughtRes(whitelistBoughtRes)
-    
-    let isGenesisHolderRes = await isGenesisHolder(exactAddress)
-    if(isGenesisHolderRes){
-      setGenesisHolder(isGenesisHolderRes)
-      genesisBoughtRes = await genesisBought(isGenesisHolderRes)
-      console.log(genesisBoughtRes, "initial res")
-      setGenesisBoughtRes(genesisBoughtRes)
+    setLoader(true);
+    let isPrivateListedRes = await isPrivateListed(exactAddress);
+    setPrivateListed(isPrivateListedRes);
+    let privateBoughtRes = await privateBought(exactAddress);
+    setPrivateBought(privateBoughtRes);
+    let isWhiteListedRes = await isWhiteListed(exactAddress);
+    setWhiteListed(isWhiteListedRes);
+    let whitelistBoughtRes = await whitelistBought(exactAddress);
+    setWhitelistBoughtRes(whitelistBoughtRes);
+
+    let isGenesisHolderRes = await isGenesisHolder(exactAddress);
+    if (isGenesisHolderRes) {
+      setGenesisHolder(isGenesisHolderRes);
+      let genesisBoughtRes = await genesisBought(isGenesisHolderRes);
+      console.log(genesisBoughtRes, "initial res");
+      setGenesisBoughtRes(genesisBoughtRes);
     }
 
-
-    //Setting text MESSAGE 
-    if(isPrivateListedRes)setTextMessage(msg.private)
-    if(isWhiteListedRes)setTextMessage(msg.whiteList)
-    if(isGenesisHolderRes)setTextMessage(msg.genesis)
+    //Setting text MESSAGE
+    if (isPrivateListedRes) setTextMessage(msg.private);
+    if (isWhiteListedRes) setTextMessage(msg.whiteList);
+    if (isGenesisHolderRes) setTextMessage(msg.genesis);
 
     // let genesisClaimed = await genesisClaimed(genesisHolder)
     // let gen2Claimed = await gen2Claimed(genesisHolder)
-    
-    setLoader(false)
-  }
 
-  console.log(genesisBoughtInitiated, "hgfdsdfghjgyftdrf")
+    setLoader(false);
+  };
 
   const connectWallet = async () => {
-    console.log("CLICKED ON Connect wallet")
-    await connectWalletHandler(setLoader, setConnected, setWalletAddress, setExactAddress)
-  }
+    console.log("CLICKED ON Connect wallet");
+    await connectWalletHandler(
+      setLoader,
+      setConnected,
+      setWalletAddress,
+      setExactAddress
+    );
+  };
 
   const msg = {
-    private: "WELCOME PRIVATE LIST MEMBER. YOU CAN MINT A GEN2 GODJIRA NOW! HOLD YOUR JIRA TO CLAIM A FREE JIRA AFTER 12 MARCH",
+    private:
+      "WELCOME PRIVATE LIST MEMBER. YOU CAN MINT A GEN2 GODJIRA NOW! HOLD YOUR JIRA TO CLAIM A FREE JIRA AFTER 12 MARCH",
     whiteList: "WELCOME WHITE LIST MEMBER. YOU CAN MINT A GEN2 GODJIRA NOW!",
-    genesis: `Welcome HODLer of Genesis Jira ${genesisHolder} . You have already minted gen2 Jiras HOLD your Genesis Jira to claim a free gen2 Jira`
-  }
+    genesis: `Welcome HODLer of Genesis Jira ${genesisHolder} . You have already minted gen2 Jiras HOLD your Genesis Jira to claim a free gen2 Jira`,
+  };
 
   const mintToken = async () => {
-    console.log("YES.....", privateListed, whiteListed, genesisHolder, privateBoughtInitiated, whitelistBoughtInitiated, genesisBoughtInitiated)
-    if (privateListed) {
-      let privateTime = isPrivateTime()
-      if(!privateTime){
-        //alert("Private sale not started.")
-        return 
+    console.log(
+      "YES.....",
+      privateListed,
+      whiteListed,
+      genesisHolder,
+      privateBoughtInitiated,
+      whitelistBoughtInitiated,
+      genesisBoughtInitiated
+    );
+   if (privateListed) {
+      console.log(privateListed, "privateListed");
+      let privateTime = isPrivateTime();
+      if (!privateTime) {
+        alert("Private sale not started.");
+        return;
       }
       if (privateBoughtInitiated) {
-        //alert("You have already minted")
+        // alert("You have already minted");
+        return true;
       } else {
-        await privateSale(exactAddress, privateListed)
-        alert("Minted Successfully");
+        await privateSale(exactAddress, privateListed);
+        //alert("Minted Successfully");
       }
     }
-    if (whiteListed) {
-      let whiteListTime = isWhiteTime()
-      if(!whiteListTime){
-        //alert("WhiteList sale not started.")
-        return 
+    else if (whiteListed) {
+      let whiteListTime = isWhiteTime();
+      if (!whiteListTime) {
+        alert("WhiteList sale not started.");
+        return;
       }
       if (whitelistBoughtInitiated) {
-        //alert("You have already minted")
+        // alert("You have already minted");
+        return true;
       } else {
-        await whitelistSale(exactAddress, whiteListed)
-        alert("Minted Successfully");
+        await whitelistSale(exactAddress, whiteListed);
+        //alert("Minted Successfully");
       }
     }
-    if (genesisHolder) {
-      let genesisTime = isGenesisTime()
-      if(genesisTime){
-        // alert("Genesis sale not started.")
-        return 
+    else if (genesisHolder) {
+      console.log(genesisHolder, "genesisHolder");
+      let genesisTime = isGenesisTime();
+      if (!genesisTime) {
+        alert("Genesis sale not started.");
+        return;
       }
-      if (genesisBoughtInitiated) {
-        // alert("You have already minted")
+      if (!genesisBoughtInitiated) {
+        // alert("You have already minted");
+        return true;
       } else {
-        await genesisSale(genesisHolder)
-        alert("Minted Successfully");
+        await genesisSale(genesisBoughtInitiated);
+        //alert("Minted Successfully");
       }
     }
   };
+  console.log(whiteListed, "whiteListed");
 
   return (
     <>
-      {loader ? <div className="loader">Loading...</div> : <div className="textContainer">
-        <div className="gen-2-logo">
-          {!connected ? <img
-            style={{ width: "450px", height: "200px", marginTop: "12%" }}
-            src={require("../assets/Group 13081@2x.png")}
-          /> : <img
-            style={{ width: "450px", height: "200px", marginLeft: "29%", marginTop: "12%" }}
-            src={require("../assets/Group 13081@2x.png")}
-          />}
-        </div>
-        <div className="connect-mint-button">
-          {connected ? (
-            false ? (
-              <div className="message">
-                YOU HAVE ALREADY MINTED
-              </div>
+      {loader ? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <div className="textContainer">
+          <div className="gen-2-logo">
+            {!connected ? (
+              <img
+                style={{ width: "450px", height: "200px", marginTop: "12%" }}
+                src={require("../assets/Group 13081@2x.png")}
+              />
             ) : (
-              <>
-                {connected && !minted && <>
-                  <div className="wallet-address">
-                    {walletAddress}
-                  </div>
-                  <div className="wallet-address-connected">
-                    CONNECTED
-                  </div>
-                  <div className="wallet-address-text">
-                    {textMessage}
-                  </div>
-                </>}
-                <button
-                  className="connect-wallet-button-mint"
-                  onClick={() => {
-                    mintToken();
-                  }}
-                >
-                  MINT NOW!
-                </button>
-                {/* <div className="number-of-mint">
-              <span className="osake-font-apply">2500 </span>LEFT
-            </div> */}
-              </>
-            )
-          ) : (
-            <button
-              className="connect-wallet-button-initial"
-              onClick={() => connectWallet()}
-            >
-              <span>
-                <img style={{ marginRight: "12px", width: "30px", height: "30px" }} src={require("../assets/wallet.png")} />
-              </span>
-              CONNECT
-            </button>
-          )}
+              <img
+                style={
+                  privateBoughtInitiated ||
+                  whitelistBoughtInitiated ||
+                  genesisBoughtInitiated
+                    ? {
+                        width: "450px",
+                        height: "200px",
+                        marginLeft: "6%",
+                        marginTop: "auto",
+                      }
+                    : {
+                        width: "450px",
+                        height: "200px",
+                        marginLeft: "29%",
+                        marginTop: "12%",
+                      }
+                }
+                src={require("../assets/Group 13081@2x.png")}
+              />
+            )}
+          </div>
+          <div className="connect-mint-button">
+            {connected ? (
+              privateListed && privateBoughtInitiated ? (
+                <div className="message">YOU HAVE ALREADY MINTED 1</div>
+              ) : whiteListed && whitelistBoughtInitiated ? (
+                <div className="message">YOU HAVE ALREADY MINTED 2</div>
+              ) : genesisHolder && !genesisBoughtInitiated ? (
+                <div className="message">YOU HAVE ALREADY MINTED 3</div>
+              ) : connected && minted ? (
+                <>
+                  <div className="wallet-address">{walletAddress}</div>
+                  <div className="wallet-address-connected">CONNECTED</div>
+                  <div className="wallet-address-text">{textMessage}</div>
+                </>
+              ) : (
+                <>
+                  {genesisBoughtInitiated &&
+                  genesisBoughtInitiated.length > 0 ? (
+                    <>
+                      <div className="wallet-address">{walletAddress}</div>
+                      <div className="wallet-address-connected">CONNECTED</div>
+                      <div className="wallet-address-text">
+                        Welcome Holder of Genesis Jira #
+                        {genesisBoughtInitiated.toString()}.
+                      </div>
+                      {privateListed && !privateBoughtInitiated && (
+                        <div className="wallet-address-text">
+                          You are also in Private list
+                        </div>
+                      )}
+                      <button
+                        className="connect-wallet-button-mint"
+                        onClick={() => {
+                          mintToken();
+                        }}
+                      >
+                        MINT NOW!
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      {whiteListed ? (
+                        <>
+                          <div className="wallet-address-text">
+                            You are in White list
+                          </div>
+                          <button
+                            className="connect-wallet-button-mint"
+                            onClick={() => {
+                              mintToken();
+                            }}
+                          >
+                            MINT NOW!
+                          </button>
+                        </>
+                      ) : (
+                        <>
+                          <div className="wallet-address">{walletAddress}</div>
+                          <div className="wallet-address-connected">
+                            CONNECTED
+                          </div>
+                          <div className="wallet-address-text">
+                            You are not eligable to mint
+                          </div>
+                        </>
+                      )}
+                    </>
+                  )}
+                </>
+              )
+            ) : (
+              <button
+                className="connect-wallet-button-initial"
+                onClick={() => connectWallet()}
+              >
+                <span>
+                  <img
+                    style={{
+                      marginRight: "12px",
+                      width: "30px",
+                      height: "30px",
+                    }}
+                    src={require("../assets/wallet.png")}
+                  />
+                </span>
+                CONNECT
+              </button>
+            )}
+            {/* {connected ? (
+              !privateBoughtInitiated ||
+              whitelistBoughtInitiated ||
+              (!genesisBoughtInitiated &&
+                genesisBoughtInitiated.length === 0) ? (
+                <div className="message">YOU HAVE ALREADY MINTED</div>
+              ) : (
+                <></>
+              )
+            ) : (
+              <button
+                className="connect-wallet-button-initial"
+                onClick={() => connectWallet()}
+              >
+                <span>
+                  <img
+                    style={{
+                      marginRight: "12px",
+                      width: "30px",
+                      height: "30px",
+                    }}
+                    src={require("../assets/wallet.png")}
+                  />
+                </span>
+                CONNECT
+              </button>
+            )} */}
+          </div>
         </div>
-      </div>}
+      )}
     </>
   );
 };
--- src/contract/contractInteraction.js
+++ src/contract/contractInteraction.js
@@ -1,231 +1,236 @@
-import { ethers } from 'ethers'
-import abi from './abi.json'
-import axios from "axios"
-import genesisabi from './genesisabi.json'
+import { ethers } from "ethers";
+import abi from "./abi.json";
+import axios from "axios";
+import genesisabi from "./genesisabi.json";
 
-const PRIVATE_TIME = 1646718358
-const WHITE_TIME = 1646718358
-const GENESIS_TIME = 1646718358
+const PRIVATE_TIME = 1646718358;
+const WHITE_TIME = 1646718358;
+const GENESIS_TIME = 1646718358;
 
+let address;
+let contractAddress;
+let contract;
+let genesisContract;
+let genesisContractAddress;
 
-let address
-let contractAddress
-let contract
-let genesisContract
-let genesisContractAddress
-
-const value = 0.099
-const backend_url = 'https://godjira-backend.herokuapp.com/'
+const value = 0.099;
+const backend_url = "https://godjira-backend.herokuapp.com/";
 export const providerHandler = async () => {
-
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const account = await provider.listAccounts();
-    address = account[0];
-    const signer = provider.getSigner();
-    contractAddress = "0xD16652fFfFc0717bDa8F02e0e1456526d820afc6"
-    genesisContractAddress = "0x2286F6EF1DcD3365a9598fbD6786abD799fE3d96"
-    contract = new ethers.Contract(contractAddress, abi, signer);
-    genesisContract = new ethers.Contract(genesisContractAddress, genesisabi, signer);
-    console.log('contracts init done')
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const account = await provider.listAccounts();
+  address = account[0];
+  const signer = provider.getSigner();
+  contractAddress = "0xD16652fFfFc0717bDa8F02e0e1456526d820afc6";
+  genesisContractAddress = "0x2286F6EF1DcD3365a9598fbD6786abD799fE3d96";
+  contract = new ethers.Contract(contractAddress, abi, signer);
+  genesisContract = new ethers.Contract(
+    genesisContractAddress,
+    genesisabi,
+    signer
+  );
+  console.log("contracts init done");
 };
 
-//wallet address 
+//wallet address
 export const getAddress = () => {
-    return address;
-}
+  return address;
+};
 
 //sale functions
 
 // sale function for private list account
 export const privateSale = async (privateListedAddress, signature) => {
-    try{
-        const n = await contract.privateSale([privateListedAddress, true, signature], {
-            value: ethers.utils.parseEther(`${value}`).toString()
-        })
-        await n.wait()
-    }catch(e){
-        alert("YOU HAVE ALREADY MINTED")
-    }
-}
+  try {
+    const n = await contract.privateSale(
+      [privateListedAddress, true, signature],
+      {
+        value: ethers.utils.parseEther(`${value}`).toString(),
+      }
+    );
+    await n.wait();
+    alert("Successfully minted")
+  } catch (e) {
+    alert("Transaction Failed")
+  }
+};
 
 // sale function for white list account
 export const whitelistSale = async (whitelistedAddress, signature) => {
-    try{
-        const n = await contract.whitelistSale([whitelistedAddress, false, signature], {
-            value: ethers.utils.parseEther(`${value}`).toString()
-        })
-        await n.wait()
-    }catch(e){
-        alert("YOU HAVE ALREADY MINTED")
-    }
-}
+  try {
+    const n = await contract.whitelistSale(
+      [whitelistedAddress, false, signature],
+      {
+        value: ethers.utils.parseEther(`${value}`).toString(),
+      }
+    );
+    await n.wait();
+    alert("Successfully minted")
+  } catch (e) {
+    alert("Transaction Failed")
+  }
+};
 
 // sale function for genesis holders (pass array of tokenIds)
 export const genesisSale = async (tokenIds) => {
-    try{
-        const n = await contract.genesisSale(tokenIds, {
-            value: ethers.utils.parseEther(`${value * tokenIds.length}`).toString()
-        })
-        await n.wait()
-    }catch(e){
-        alert("YOU HAVE ALREADY MINTED")
-    }
-}
+  try {
+    const n = await contract.genesisSale(tokenIds, {
+      value: ethers.utils
+        .parseEther(`${value * tokenIds.length * 2}`)
+        .toString(),
+    });
+    await n.wait();
+    alert("Successfully minted")
+  } catch (e) {
+    alert("Transaction Failed")
+  }
+};
 
 //claim functions
 
 //claim function for private sales buyers (pass array of tokenIds)
 export const privateSalesClaim = async (tokenIds) => {
-    const n = await contract.privateSale(tokenIds)
-    await n.wait()
-}
+  const n = await contract.privateSale(tokenIds);
+  await n.wait();
+};
 
 //claim function for genesis holders (pass array of tokenIds)
 export const genesisClaim = async (tokenIds) => {
-    const n = await contract.genesisClaim(tokenIds)
-    await n.wait()
-}
+  const n = await contract.genesisClaim(tokenIds);
+  await n.wait();
+};
 
 //mapping functions
 
 //sale mapping
 // wallet already bought from private sale? return bool
 export const privateBought = async (walletAddress) => {
-    const n = await contract.privateBought(walletAddress)
-    return n;
-}
+  const n = await contract.privateBought(walletAddress);
+  return n;
+};
 
 // wallet already bought from genesis sale? return bool
 export const genesisBought = async (tokenIds) => {
-    
-    let unclaimed = []
-    for (let i in tokenIds) {   
-        const n = await contract.genesisBought(tokenIds[i])
-        if(!n){
-            unclaimed.push(tokenIds[i])
-        }
+  let unclaimed = [];
+  for (let i in tokenIds) {
+    const n = await contract.genesisBought(tokenIds[i]);
+    if (!n) {
+      unclaimed.push(tokenIds[i]);
     }
-    console.log(unclaimed)
-    return unclaimed
-    
-    
-}
+  }
+  console.log(unclaimed);
+  return unclaimed;
+};
 
 // wallet already bought from whitelistBought sale? return bool
 export const whitelistBought = async (walletAddress) => {
-    const n = await contract.whitelistBought(walletAddress)
-    return n;
-}
+  const n = await contract.whitelistBought(walletAddress);
+  return n;
+};
 
 //claim mapping
 //wallet already claimed genesis? return bool
 export const genesisClaimed = async (tokenId) => {
-    const n = await contract.genesisClaimed(tokenId)
-    return n;
-}
+  const n = await contract.genesisClaimed(tokenId);
+  return n;
+};
 
 //wallet already claimed gen2? return bool
 export const gen2Claimed = async (tokenId) => {
-    const n = await contract.gen2Claimed(tokenId)
-    return n;
-}
+  const n = await contract.gen2Claimed(tokenId);
+  return n;
+};
 
 //is in list checker functions
 // is in whitelist?
 
 export const isWhiteListed = async (walletAddress) => {
-    try {
-        const { data } = await axios.post(backend_url + "whitelist", {
-            address: walletAddress
-        })
-        return data.signature
-
-    }
-    catch (err) {
-        console.log(err)
-        return false
-    }
-
-}
+  try {
+    const { data } = await axios.post(backend_url + "whitelist", {
+      address: walletAddress,
+    });
+    return data.signature;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
 
 // is in privatelist?
 export const isPrivateListed = async (walletAddress) => {
-    console.log('calling isPrivateListed', walletAddress)
-    try {
-        const { data } = await axios.post(backend_url + "privatelist", {
-            address: walletAddress
-        })
-        //console.log(data.signature)
-        return data.signature
-
-    }
-    catch (err) {
-        console.log(err)
-        return false
-    }
-
-}
+  console.log("calling isPrivateListed", walletAddress);
+  try {
+    const { data } = await axios.post(backend_url + "privatelist", {
+      address: walletAddress,
+    });
+    //console.log(data.signature)
+    return data.signature;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
 
 export const isGenesisHolder = async (walletAddress) => {
-    const nftCount = await genesisContract.balanceOf(walletAddress)
-    console.log(nftCount.toNumber())
-    if (nftCount.toNumber() === 0) {
-        return false
+  const nftCount = await genesisContract.balanceOf(walletAddress);
+  console.log(nftCount.toNumber());
+  if (nftCount.toNumber() === 0) {
+    return false;
+  } else {
+    let nft_list = [];
+    for (let i = 0; i < nftCount.toNumber(); i++) {
+      const n = await genesisContract.tokenOfOwnerByIndex(walletAddress, i);
+      nft_list.push(n.toNumber());
     }
-    else {
-        let nft_list = []
-        for (let i = 0; i < nftCount.toNumber(); i++) {
-            const n = await genesisContract.tokenOfOwnerByIndex(walletAddress, i)
-            nft_list.push(n.toNumber())
-        }
-        console.log(nft_list)
-        return nft_list
-    }
-}
+    console.log(nft_list);
+    return nft_list;
+  }
+};
 
 export const isPrivateTime = () => {
-    if (Math.floor(Date.now() / 1000) >= PRIVATE_TIME) {
-        return true
-    }
-    else {
-        return false
-    }
-}
+  if (Math.floor(Date.now() / 1000) >= PRIVATE_TIME) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 export const isWhiteTime = () => {
-    if (Math.floor(Date.now() / 1000) >= WHITE_TIME) {
-        return true
-    }
-    else {
-        return false
-    }
-}
+  if (Math.floor(Date.now() / 1000) >= WHITE_TIME) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 export const isGenesisTime = () => {
-    if (Math.floor(Date.now() / 1000) >= GENESIS_TIME) {
-        return true
-    }
-    else {
-        return false
-    }
-}
+  if (Math.floor(Date.now() / 1000) >= GENESIS_TIME) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-export const connectWalletHandler = async (setLoader, setConnected, setWalletAddress, setExactAddress) => {
-    setLoader(true);
-    if (window.ethereum) {
-        window.ethereum
-            .request({ method: "eth_requestAccounts" })
-            .then(async (result) => {
-                await providerHandler();
-                const addr = getAddress()
-                setExactAddress(addr)
-                console.log(addr)
-                let strFirstThree = addr.substring(0, 5);
-                let strLastThree = addr.substr(addr.length - 5);
-                let address = `${strFirstThree}...${strLastThree}`;
-                setWalletAddress(address);
-                setConnected(true);
-                setLoader(false)
-            })
-    }
+export const connectWalletHandler = async (
+  setLoader,
+  setConnected,
+  setWalletAddress,
+  setExactAddress
+) => {
+  setLoader(true);
+  if (window.ethereum) {
+    window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then(async (result) => {
+        await providerHandler();
+        const addr = getAddress();
+        setExactAddress(addr);
+        console.log(addr);
+        let strFirstThree = addr.substring(0, 5);
+        let strLastThree = addr.substr(addr.length - 5);
+        let address = `${strFirstThree}...${strLastThree}`;
+        setWalletAddress(address);
+        setConnected(true);
+        setLoader(false);
+      });
+  }
 };
